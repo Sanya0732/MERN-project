9ea6c12ebbdf58307bf2b8d9acae4993790bd3a9
Sanya Srivastava <129599279+Sanya0732@users.noreply.github.com>
2024-02-24 16:39:34 +0530

    Add files via upload

---
+++ js/lecture3/operator.js
@@ -0,0 +1,52 @@
+"harrybhai"; //no error
+console.log("OPERATORS"    );
+let a = 45;
+let b = 4;
+console.log("a+b = ",a+b  );
+console.log("a+b = ",a-b  );
+console.log("a+b = ",a/b  );
+
+console.log("a+b = ",a*b  );
+console.log("a**b = ",a**b  );
+console.log("a%b = ",a%b  );
+console.log("++a = ",++a  );
+console.log("a = ",a  );
+console.log("a-- = ",a-- );
+console.log("--a = ",--a );
+//assign op
+let z = 9;
+z+= 1;
+console.log(z);
+
+//comparison operator 
+let comp1 = 6;
+let comp2 = 7;
+console.log(" is comp1 == comp2 ?",comp1==comp2);
+console.log(" is comp1 != comp2 ?",comp1!=comp2);
+// difference b/w '== , === ' or '!= , !== '
+let comp3 = 6;
+let comp4 = "6";
+console.log(" is comp3 === comp4 ?",comp3===comp4);
+console.log(" is comp3 !== comp4 ?",comp3!==comp4);
+
+let comp5 = 6;
+let comp6 = "6";
+console.log(" is comp5 == comp6 ?",comp5==comp6);
+console.log(" is comp5 != comp6 ?",comp5!=comp6);
+//logical operator 
+let x =5;
+let y =6;
+console.log(x<y && x==5);
+console.log(x>y || x==5);
+console.log(!true);
+
+
+
+
+
+
+
+
+
+
+
