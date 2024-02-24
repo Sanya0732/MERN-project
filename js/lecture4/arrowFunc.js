0b6d80b65772bf210ae7680dbe06aa90c528e9d1
Sanya Srivastava <129599279+Sanya0732@users.noreply.github.com>
2024-02-24 17:07:52 +0530

    Add files via upload

---
+++ js/lecture4/arrowFunc.js
@@ -0,0 +1,13 @@
+// const sayhello = function(){
+//     console.log(" haahhahaha ");
+// }
+// sayhello();
+
+const sayhello = ()=>{
+    console.log(" haahhahaha ");
+}
+sayhello();
+//one liner do not require braces , automatic return
+// const greet = ()=>  "hehe";
+const greet = (name,endname) => "good morning "+name + endname;
+console.log(greet("harry","code"));
